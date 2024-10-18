386f45bdc1347df987b2c191e8aa518cef40ca94
mBentaifour <bentaifourmoh@gmail.com>
2024-10-17 17:27:39 -0700

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" Script that computes a minimum operations
-    needed in a CopyAll - Paste task
+"""script that computes a minimum operations
+needed in a CopyAll - Paste task
 """
 
 
 def minOperations(n):
-
     """
     Method for compute the minimum number
     of operations for task Copy All and Paste
@@ -25,4 +24,4 @@ def minOperations(n):
             while n % i == 0:
                 n /= i
                 factor_list.append(i)
-    return sum(factor_list) 
+    return sum(factor_list)
