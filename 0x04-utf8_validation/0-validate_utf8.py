d232d46e09ce6fc5028b9e90ce2db14435a7ad7a
mbentaifour <bentaifourmoh@gmail.com>
2024-10-31 19:08:43 +0000

    UTF-8 une norme de codage de caracteres

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,6 @@
 UTF-8 Validation
 
 a method that determines if a given data set represents a valid UTF-8 encoding
-
 """
 
 
@@ -17,6 +16,7 @@ def validUTF8(data):
 
     Prototype: def validUTF8(data)
     Return: True if data is a valid UTF-8 encoding, else return False
+
     A character in UTF-8 can be 1 to 4 bytes long
     The data set can contain multiple characters
     The data will be represented by a list of integers
