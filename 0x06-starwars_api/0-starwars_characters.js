d3809e32d26d7f58afbddab20e3a6e44d0327d99
mbentaifour <bentaifourmoh@gmail.com>
2024-11-15 13:34:53 +0000

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,39 @@
 #!/usr/bin/node
 
-
 const request = require('request');
-
 const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+// URL du film correspondant à l'ID
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    return;
   }
+
+  const filmData = JSON.parse(body);
+
+  if (!filmData.characters) {
+    console.error('No characters found for this movie.');
+    return;
+  }
+
+  // Itère à travers chaque URL de personnage et affiche le nom
+  filmData.characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
 });
