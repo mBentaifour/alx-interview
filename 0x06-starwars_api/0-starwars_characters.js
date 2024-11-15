9a1d8e844109683fb72e92ca8d709cb575a077f9
mbentaifour <bentaifourmoh@gmail.com>
2024-11-15 14:10:50 +0000

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,21 @@
 #!/usr/bin/node
 
-const request = require('request');
-const movieId = process.argv[2];
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
 
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
+
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
 }
 
-// URL du film correspondant à l'ID
-const url = `https://swapi.dev/api/films/${movieId}/`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  const filmData = JSON.parse(body);
-
-  if (!filmData.characters) {
-    console.error('No characters found for this movie.');
-    return;
-  }
-
-  // Itère à travers chaque URL de personnage et affiche le nom
-  const printCharacterName = (characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  };
-
-  // Utilise `forEach` pour chaque personnage de la liste
-  filmData.characters.forEach(printCharacterName);
-});
+starwarsCharacters(filmID);
